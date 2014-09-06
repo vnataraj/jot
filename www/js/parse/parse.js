2aa7dd9e3a292d8388d0e9c2bdd610315fba8de5
Vipul Nataraj <vnataraj@purdue.edu>
2014-09-06 03:48:42 -0400

    ability to create users.  still kind of buggy

--- www/js/parse/parse.js
+++ www/js/parse/parse.js
@@ -1,39 +1,38 @@
 function create(username, password, email){
-
   var user = new Parse.User();
   user.set("username", username);
   user.set("password", password);
   user.set("email", email);
-
+  alert("here's a user!");
   // other fields can be set just like with Parse.Object
   user.signUp(null, {
     success: function(user) {
-      loginManager(logout(), null);
+      $('#create').submit();
     },
     error: function(user, error) {
       // Show the error message somewhere and let the user try again.
       alert("Error: " + error.code + " " + error.message);
     }
   });
-}
+};
 
 
 function login(username, password){
   Parse.User.logIn("myname", "mypass", {
     success: function(user) {
-      Do stuff after successful login.
+      loginManager(logout(), null);
     },
     error: function(user, error) {
       // The login failed. Check error to see why.
     }
   });
 
-}
+};
 
 function logout(){
   Parse.User.logOut();
   var currentUser = Parse.User.current();
-}
+};
 
 function loginManager(func, callback){
   setTimeout(function() {
@@ -41,4 +40,4 @@ function loginManager(func, callback){
     if(callback){callback();}
   }, 5400000);
 
-}
+};
